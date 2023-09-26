1352a7b7d42bef6c00c87b156ad769d7555280f9
vinhhuynh1 <vinhhuynh173@gmail.com>
2023-09-25 20:01:50 -0600

    Final update
    Update app folder

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Name: Vinh Huynh</p>
+            <p>Course section: CPRG 306 E</p>
+            <Link href="https://github.com/vinhhuynh1">https://github.com/vinhhuynh1</Link>   
+        </main>
+    )
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo"
+
+export default function Week2() {
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </main>    
+    )
+}
