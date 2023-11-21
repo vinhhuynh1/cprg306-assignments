906e502db518d826ad0206f1271175f2739f9676
vinhhuynh1 <vinhhuynh173@gmail.com>
2023-11-20 23:58:04 -0700

    Update Week 10

--- app/week8/shopping-list/item.js
+++ app/week8/shopping-list/item.js
@@ -6,8 +6,8 @@ function Item ({item, onSelect}) {
     return ( 
         <main class="font-mono text-sm p-2 m-2 bg-sky-400 max-w-sm border-x-8 border-y-8 text-white hover:bg-blue-500 cursor-pointer" onClick={handleClick}>
 
-            <h2 class="text-xl font-bold uppercase">{item.data.name}</h2>
-            <li class="text-xm">Buy {item.data.quantity} in {item.data.category}</li>
+            <h2 class="text-xl font-bold uppercase">{item.name}</h2>
+            <li class="text-xm">Buy {item.quantity} in {item.category}</li>
                 
         </main>
     )
